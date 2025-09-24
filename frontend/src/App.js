5e7b5a31791c31cc0bc14ee49cfe2436487b0cd5
serina <153125049+slimchouaib1@users.noreply.github.com>
2025-09-24 19:50:33 +0100

    Workspace

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+// ...existing code...
+function App() {
+  return (
+    <Routes>
+      {/* Home and Result pages will be added here */}
+    </Routes>
+  );
+}
+export default App;
